0a2644c0d25c2419a00809c5ed36b72c0725c6f0
moha0047 <imdifferent.hassler@gmail.com>
2017-09-07 21:03:24 +0200

    reset of commit and removed imports

--- src/projectrussian/FXMLTestController.java
+++ src/projectrussian/FXMLTestController.java
@@ -14,7 +14,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * FXML Controller class
