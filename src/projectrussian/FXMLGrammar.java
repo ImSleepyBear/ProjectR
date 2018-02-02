04ff533a1b6a0e4e778f51980870f31c948d4c4b
moha0047 <imdifferent.hassler@gmail.com>
2018-02-02 23:53:13 +0100

    added more items to the grammar layout as well as some functionality in the corresponding controller, this mostly regards functionalities that are needed in a few out of many situations that will show up later in development

--- src/projectrussian/FXMLGrammar.java
+++ src/projectrussian/FXMLGrammar.java
@@ -10,6 +10,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 /**
  *
@@ -24,8 +30,55 @@ public class FXMLGrammar implements Initializable {
     private ListView<String> grammarTopics;
     
     @FXML
+    private TextFlow textflow;
+    
+    @FXML
+    private TableView tableView;
+    
+    @FXML
+    private TableColumn column1;
+    @FXML
+    private TableColumn column2;
+    @FXML
+    private TableColumn column3;
+    @FXML
+    private TableColumn column4;
+    @FXML
+    private TableColumn column5;
+    @FXML
+    private TableColumn column6;
+    
+    @FXML
+    private MenuButton menuButtonGender;
+    
+    @FXML
+    private MenuItem genderItem1;
+    @FXML
+    private MenuItem genderItem2;
+    @FXML
+    private MenuItem genderItem3;
+    
+    @FXML
+    private MenuButton menuButtonMultiples;
+    
+    @FXML
+    private MenuItem multiplierItem1;
+    @FXML
+    private MenuItem multiplierItem2;
+    
+    private boolean visibility = false;
+    
+    @FXML
     private void btnReturn(ActionEvent event){
         cs.changeScene(event, listStorage.sceneDestinations[listStorage.sceneDestinations.length-1]);
+    }
+    
+    @FXML
+    private void tableVisibility(){
+        visibility = !visibility;
+        tableView.setVisible(visibility);
+        menuButtonGender.setVisible(visibility);
+        menuButtonMultiples.setVisible(visibility);
     }
     
     @Override
@@ -35,6 +88,25 @@ public class FXMLGrammar implements Initializable {
         obsList.addAll(Arrays.asList(listStorage.grammarListview));
         
         grammarTopics.setItems(obsList);
+
+        tableView.setVisible(visibility);
+        menuButtonGender.setVisible(visibility);
+        menuButtonMultiples.setVisible(visibility);
+        
+        menuButtonGender.getItems().addAll(FXCollections.observableArrayList(
+                genderItem1 = new MenuItem(listStorage.genders[0]),
+                genderItem2 = new MenuItem(listStorage.genders[1]),
+                genderItem3 = new MenuItem(listStorage.genders[2])
+        ));
+        
+        menuButtonMultiples.getItems().addAll(FXCollections.observableArrayList(
+                multiplierItem1 = new MenuItem(listStorage.singularPlural[0]),
+                multiplierItem2 = new MenuItem(listStorage.singularPlural[1])
+        ));
+        
+        Text text = new Text();
+        text.setText("Hello World!");
+        textflow.getChildren().addAll(text);
     }
     
 }
